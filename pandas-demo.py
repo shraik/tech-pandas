a6c9d52dfbf214429a2f7ae1fd5c34d106b32c4c
shraik <shraikus@gmail.com>
2024-03-03 05:25:08 +0000

    up2

--- pandas-demo.py
+++ pandas-demo.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 df= pd.read_excel("demodata/SampleData.xlsx",sheet_name='SalesOrders')  
 print(df)
+
+writer= pd.ExcelWriter("path_to_file.xlsx",
+    mode="a",
+    engine="openpyxl",
+    if_sheet_exists="overlay",)
+
+df.to_excel(writer, sheet_name="Sheet1", startcol=3)
