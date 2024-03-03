188620eec3ee7b9229467409a7f1e1d9b2c5e142
shraik <shraikus@gmail.com>
2024-03-03 10:17:12 +0000

    3

--- pandas-demo.py
+++ pandas-demo.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
-df= pd.read_excel("demodata/SampleData.xlsx",sheet_name='SalesOrders')  
-print(df)
+df = pd.read_excel("demodata/SampleData.xlsx", sheet_name="SalesOrders")
+print(df.head())
 
-writer= pd.ExcelWriter("path_to_file.xlsx",
-    mode="a",
+writer = pd.ExcelWriter(
+    "demodata/out_to_file.xlsx",
+    date_format="YYYY-MM-DD",
+    datetime_format="YYYY-MM-DD HH:MM:SS",
     engine="openpyxl",
-    if_sheet_exists="overlay",)
+)
 
 df.to_excel(writer, sheet_name="Sheet1", startcol=3)
+
+writer.close()
+print("Okay, exit(0)")
